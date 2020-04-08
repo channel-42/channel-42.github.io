3513dbc9eb0d4d13f20f3fc429cbabc6733ca0f0
channel-42 <59137829+channel-42@users.noreply.github.com>
2020-04-08 12:31:41 +0200

    Responsive design near final

--- js/mobile_men.js
+++ js/mobile_men.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
   $('.fa-bars').on('click', function() {
     $('header nav ul').toggleClass('open');
+    $('#progress-bar').toggleClass('open');
+    $('#scroll-ind').toggleClass('open');
+    $('#logo img').toggleClass('open');
+  });
+  $('header nav ul li').on('click', function() {
+    $('header nav ul').toggleClass('open');
+    $('#scroll-ind').toggleClass('open');
+    $('#logo img').toggleClass('open');
   });
 });
